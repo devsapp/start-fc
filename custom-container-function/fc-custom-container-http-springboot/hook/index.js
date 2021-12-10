263507b3b48a6b4ff47df55d4445ac486da90dc0
anycodes <service@52exe.cn>
2021-12-10 14:53:07 +0800

    update help

--- custom-container-function/fc-custom-container-http-springboot/hook/index.js
+++ custom-container-function/fc-custom-container-http-springboot/hook/index.js
@@ -1,23 +1,23 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     ____  _     _ ___  _ _     _        _____ ____ 
     /  _ \\/ \\   / \\\\  \\/// \\ /\\/ \\  /|  /    //   _\\
     | / \\|| |   | | \\  / | | ||| |\\ ||  |  __\\|  /  
     | |-||| |_/\\| | / /  | \\_/|| | \\||  | |   |  \\__
-    \\_/ \\|\\____/\\_//_/   \\____/\\_/  \\|  \\_/   \\____/
-                                                
-                                        `)
+    \\_/ \\|\\____/\\_//_/   \\____/\\_/  \\|  \\_/   \\____/`)
+}
+
+async function postInit(inputObj) {
     console.log(`\n    Welcome to the Aliyun FC start application
      This application requires to open these services: 
          FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the aliyun fc project.
-     The application uses FC component：
-        * Help Docs  : https://github.com/devsapp/fc
-        * Yaml Config: https://github.com/devsapp/fc/blob/main/docs/zh/yaml.md
-     The application homepage: https://github.com/devsapp/start-fc\n`)
+         ACR: https://cr.console.aliyun.com/
+     
+     * 额外说明：s.yaml中声明了actions：
+        部署前执行：s build --use-docker --dockerfile ./code/Dockerfile
+       如果不需要每次都构建项目，或者部署前不需要构建，或者已经手动构建了，可以注释掉这部分内容
+       > PS：部署的时候还需要修改s.yaml中image字段为自己的acr配置的地址
+     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署
+     \n`)
 }
 
 module.exports = {
--- custom-function/java8/fc-custom-java8-http/hook/index.js
+++ custom-function/java8/fc-custom-java8-http/hook/index.js
@@ -1,26 +1,19 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     ____  _     _ ___  _ _     _        _____ ____ 
     /  _ \\/ \\   / \\\\  \\/// \\ /\\/ \\  /|  /    //   _\\
     | / \\|| |   | | \\  / | | ||| |\\ ||  |  __\\|  /  
     | |-||| |_/\\| | / /  | \\_/|| | \\||  | |   |  \\__
-    \\_/ \\|\\____/\\_//_/   \\____/\\_/  \\|  \\_/   \\____/
-                                                
-                                        `)
+    \\_/ \\|\\____/\\_//_/   \\____/\\_/  \\|  \\_/   \\____/`)
+}
+
+async function postInit(inputObj) {
     console.log(`\n    Welcome to the Aliyun FC start application
      This application requires to open these services: 
          FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the aliyun fc project.
-     The application uses FC component：
-        * Help Docs  : https://github.com/devsapp/fc
-        * Yaml Config: https://github.com/devsapp/fc/blob/main/docs/zh/yaml.md
-     The application homepage: https://github.com/devsapp/start-fc\n`)
-}
-
-module.exports = {
-    postInit,
-    preInit
+     
+     * 额外说明：s.yaml中声明了actions：
+        部署前执行：mvn package
+       如果不需要每次都构建项目，或者部署前不需要构建，或者已经手动构建了，可以注释掉这部分内容
+     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署
+     \n`)
 }
