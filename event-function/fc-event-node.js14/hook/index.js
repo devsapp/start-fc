56e701d48fd1e583248d8d803d25f7c2bd70d7c8
yuankun.yk <yuankun.yk@alibaba-inc.com>
2022-01-05 01:42:51 +0800

    feat: 增加 fc-event-node.js14 demo

---
+++ event-function/fc-event-node.js14/hook/index.js
@@ -0,0 +1,21 @@
+async function preInit(inputObj) {
+    console.log(`\n     ____  _     _ ___  _ _     _        _____ ____ 
+    /  _ \\/ \\   / \\\\  \\/// \\ /\\/ \\  /|  /    //   _\\
+    | / \\|| |   | | \\  / | | ||| |\\ ||  |  __\\|  /  
+    | |-||| |_/\\| | / /  | \\_/|| | \\||  | |   |  \\__
+    \\_/ \\|\\____/\\_//_/   \\____/\\_/  \\|  \\_/   \\____/`)
+}
+
+async function postInit(inputObj) {
+    console.log(`\n    Welcome to the Aliyun FC start application
+     This application requires to open these services: 
+         FC : https://fc.console.aliyun.com/
+     
+     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署
+     \n`)
+}
+
+module.exports = {
+    postInit,
+    preInit
+}
