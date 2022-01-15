6607370c03a3263d8da0c459634e7ad7d84261d3
rsonghuster <ls_huster@163.com>
2022-01-15 22:28:29 +0800

    add async task with python

---
+++ async-task/python3/src/code/dest-fail/index.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import logging
+
+# To enable the initializer feature (https://help.aliyun.com/document_detail/158208.html)
+# please implement the initializer function as below：
+# def initializer(context):
+#   logger = logging.getLogger()
+#   logger.info('initializing')
+
+
+def handler(event, context):
+    logger = logging.getLogger()
+    logger.info('destnation fail: {}'.format(event))
+    return {}
