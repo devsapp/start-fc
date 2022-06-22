2ec5106601c95a9d43b18e8784e588cce770d16f
lowkeyrd <wangrenda1987@gmail.com>
2022-06-22 19:52:12 +0800

    fix maven problem with java17

--- custom-function/java17/fc-custom-java17-http/src/code/src/test/java/com/example/demo/DemoApplicationTests.java
+++ custom-function/java17/fc-custom-java17-http/src/code/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,13 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	public void testSayHello() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
 }
