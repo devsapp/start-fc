3bc775b30932ef2a43e3c6139fbe8fc5091f888c
luoni.fz <luoni.fz@alibaba-inc.com>
2022-11-03 23:20:01 +0800

    update golang demo

--- event-function/fc-event-golang1.x/src/code/main.go
+++ event-function/fc-event-golang1.x/src/code/main.go
@@ -10,16 +10,8 @@ func main() {
 	fc.Start(HandleRequest)
 }
 
-/*
-event:
-{
-    "key1": "value1",
-    "key2": "value2",
-    "key3": "value3"
-}
-*/
-func HandleRequest(event map[string]interface{}) (string, error) {
-	fmt.Printf("event: %v\n", event)
+func HandleRequest(event []byte) (string, error) {
+	fmt.Printf("event: %s\n", string(event))
 	fmt.Println("hello world! 你好，世界!")
 	return "hello world! 你好，世界!", nil
 }
