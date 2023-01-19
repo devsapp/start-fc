f4ac84ebc8ae2613b11e6c4c59d7c1f9402f6216
rsonghuster <ls_huster@163.com>
2023-01-19 14:37:58 +0800

    add custom go http example

---
+++ custom-function/golang/fc-custom-golang-http/hook/index.js
@@ -0,0 +1,23 @@
+async function preInit(inputObj) {
+    console.log(`
+  Serverless Devs Application Case
+    
+    Cloud services required：
+    - FC : https://fc.console.aliyun.com/
+    
+    Tips：
+    - FC Component: https://www.serverless-devs.com/fc/readme`)
+}
+
+async function postInit(inputObj) {
+    console.log(`
+    * Before using, please check whether the actions command in Yaml file is available
+    * Carefully reading the notes in s.yaml is helpful for the use of the tool
+    * If need help in the use process, please apply to join the Dingtalk Group: 33947367
+    `)
+}
+
+module.exports = {
+    postInit,
+    preInit
+}
