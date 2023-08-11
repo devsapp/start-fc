d3ef2ab7567bef09c09b668e5c442e92b3af92c1
starink <shiyifan198@163.com>
2023-08-11 17:30:10 +0800

    create fc-java

---
+++ fc-java/src/src/main/java /example/App.java
@@ -0,0 +1,25 @@
+package example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.StreamRequestHandler;
+import com.aliyun.fc.runtime.FunctionInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App implements StreamRequestHandler, FunctionInitializer {
+
+    public void initialize(Context context) throws IOException {
+        // TODO
+    }
+
+    public void handleRequest(
+            InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        outputStream.write(new String("hello world\n").getBytes());
+    }
+}
