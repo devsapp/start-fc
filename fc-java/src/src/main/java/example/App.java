7686373cf8024e039ccad3ce98086896058cdba4
rsonghuster <ls_huster@163.com>
2023-08-15 20:14:38 +0800

    fix java template

--- fc-java/src/src/main/java/example/App.java
+++ fc-java/src/src/main/java/example/App.java
@@ -1,5 +1,6 @@
 package example;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,8 +19,19 @@ public class App implements StreamRequestHandler, FunctionInitializer {
         // TODO
     }
 
+    @Override
     public void handleRequest(
             InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        outputStream.write(new String("hello world\n").getBytes());
+        outputStream.write(new String("hello world " + this.convert(inputStream)).getBytes());
+    }
+
+    public String convert(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString("UTF-8");
     }
 }
